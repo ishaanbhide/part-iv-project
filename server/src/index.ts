291538f126359792e9f74f630ecd1e588c3b07d3
Matthew Ouyang <mouyang2001@gmail.com>
2023-06-18 22:34:59 +1200

    setup mongodb and basic files

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
 import api from "./routes";
+import mongoose from "mongoose";
+import path from "path";
 
 const cors = require("cors");
 
@@ -13,4 +15,21 @@ app.use(cors());
 // routes
 app.use("/api", api);
 
-app.listen(PORT, () => console.log(`App server listening on port ${PORT}`));
+// serve static files in production
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../../client/build")));
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "../../client/build/index.html"));
+    });
+}
+
+// mongodb connection
+console.log("Connecting to database...");
+mongoose
+    .connect(process.env.MONGODB_CONNECTION_STRING + "disastermap" || "")
+    .then(() => {
+        console.log("Database connected!");
+        app.listen(PORT, () =>
+            console.log(`App server listening on port ${PORT}`)
+        );
+    });
