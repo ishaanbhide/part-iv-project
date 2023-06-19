3e77d35912566788e4d362e38c1920237263e4c8
Matthew Ouyang <mouyang2001@gmail.com>
2023-06-19 18:10:05 +1200

    create news model with geojson indexing

---
+++ server/src/models/news.model.ts
@@ -0,0 +1,41 @@
+import mongoose, { Schema, Document } from "mongoose";
+
+interface INews extends Document {
+    title: string;
+    description: string;
+    source: string;
+    image: string;
+    location: {
+        type: string;
+        coordinates: number[];
+    };
+}
+
+const PointSchema: Schema = new mongoose.Schema({
+    type: {
+        type: String,
+        enum: ["Point"],
+        required: true,
+    },
+    coordinates: {
+        type: [Number],
+        required: true,
+    },
+});
+
+const NewsSchema: Schema = new Schema(
+    {
+        title: { type: String, required: true },
+        description: { type: String, required: true },
+        source: { type: String, required: true },
+        image: { type: String, required: false },
+        location: {
+            type: PointSchema,
+            index: "2dsphere",
+            required: true,
+        },
+    },
+    { timestamps: true }
+);
+
+export default mongoose.model<INews>("News", NewsSchema);
