509719d2b0a59c543133c7b3b9dc7b40d4bb90cc
Matthew Ouyang <mouyang2001@gmail.com>
2023-06-28 19:08:53 +1200

    chatgpt classification algorithm

--- pipeline/main.py
+++ pipeline/main.py
@@ -1,14 +1,39 @@
+import re
+
 import feedparser
 import openai
 import requests
 import spacy
 from bs4 import BeautifulSoup
+from geopy.geocoders import Nominatim
 
 NZ_HERALD_RSS = "https://www.nzherald.co.nz/arc/outboundfeeds/rss/section/nz/?outputType=xml&_website=nzh"
-TEST_LINK = "https://www.nzherald.co.nz/nz/power-lines-strewn-across-road-after-mount-maunganui-crash/JXOQDEMLHZDG3P7XCXTY64HAUY/"
+TEST_LINK = "https://www.nzherald.co.nz/nz/auckland-weather-surface-flooding-near-harbour-bridge-as-heavy-rain-begins/MRKSSJZE3FGHTHHBGGOFNMX3MA/"
 
 openai.api_key = "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"
 nlp = spacy.load("en_core_web_sm")
+geocoder = Nominatim(user_agent="my_app")
+
+
+def nzherald_rss():
+    feed = feedparser.parse(NZ_HERALD_RSS)
+    disasters = []
+    for entry in feed.entries:
+        clean_title = re.sub(r"[^a-zA-Z0-9\s]", "", entry.title)
+        prompt = f'"""{clean_title}""" classify news headline into NATURAL DISASTER, ACCIDENTS or OTHER'
+        completion = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[{"role": "user", "content": prompt}],
+            max_tokens=10,
+        )
+
+        print(clean_title, ",", completion.choices[0].message.content)
+        response = completion.choices[0].message.content.upper()
+
+        if "NATURAL DISASTER" in response or "ACCIDENTS" in response:
+            disasters.append((entry.title, entry.link))
+
+    return disasters
 
 
 def extract(link: str):
@@ -20,30 +45,31 @@ def extract(link: str):
     for paragraph in text:
         article += paragraph.text + " "
 
+    doc = nlp(article)
+
+    print(article)
+
     completion = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=[
             {"role": "user",
-             "content": f"Extract addresses, streets, locations from '{article}' and return as a python array"},
+             "content": f'extract locations from the article delimited by triple quotes: """${article}"""'},
         ]
     )
 
     try:
-        locations = eval(completion.choices[0].message.content)
+        content = completion.choices[0].message.content
+        locations = [item.strip("- ") for item in content.split("\n")]
         print(locations)
+
+
     except Exception as e:
         print("Error occurred while converting chatgpt content to array", str(e))
 
 
 def main():
-    feed = feedparser.parse(NZ_HERALD_RSS)
-    for entry in feed.entries:
-        print('Post Title :', entry.title)
-        print('Post Summary :', entry.summary)
-        print('Post Link :', entry.link)
-        print('------------------------')
-
-    extract(TEST_LINK)
+    disasters = nzherald_rss()
+    print(disasters)
 
 
 if __name__ == "__main__":
