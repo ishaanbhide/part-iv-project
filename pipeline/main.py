dc728e58ff6cac6591626e51bad88eef033fa211
Matthew Ouyang <mouyang2001@gmail.com>
2023-06-30 21:25:42 +1200

    tidy up code

--- pipeline/main.py
+++ pipeline/main.py
@@ -12,6 +12,8 @@ openai.api_key = "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"
 nlp = spacy.load("en_core_web_sm")
 geocoder = Nominatim(user_agent="my_app")
 
+ARTICLE_WORD_LIMIT = 100
+
 
 def get_disasters():
     feed = feedparser.parse(NZ_HERALD_RSS)
@@ -20,8 +22,8 @@ def get_disasters():
 
     for entry in feed.entries:
         clean_title = re.sub(r"[^a-zA-Z0-9\s]", "", entry.title)
-        prompt = f'"""{clean_title}""" classify news headline into NATURAL DISASTER, ACCIDENTS, WEATHER or OTHER'
 
+        prompt = f'"""{clean_title}""" classify news headline into NATURAL DISASTER, ACCIDENTS, WEATHER or OTHER'
         completion = openai.ChatCompletion.create(
             model="gpt-3.5-turbo",
             messages=[{"role": "user", "content": prompt}],
@@ -48,21 +50,23 @@ def extract_locations(title: str, link: str):
     soup = BeautifulSoup(r.text, 'html.parser')
 
     text = soup.find_all('p')
-    article = title + ":" + " ".join([paragraph.text for paragraph in text])
+    article_raw = title + " ".join([paragraph.text for paragraph in text])
+    article_truncated = " ".join(article_raw.split()[:ARTICLE_WORD_LIMIT])
+    clean_article = re.sub(r"[^a-zA-Z0-9\s]", "", article_truncated)
 
-    prompt = f'"""{article}""" extract location data from text as bullet points'
+    prompt = f'"""{clean_article}""" extract location data from text as bullet points'
     completion = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=[{"role": "user", "content": prompt}],
-        max_tokens=20,
+        max_tokens=30,
         temperature=0,
     )
 
     locations = re.findall(r'- (.+)', completion.choices[0].message.content)
 
-    print(locations)
     print(completion.choices[0].message.content)
     print("Total tokens used:", completion.usage.total_tokens)
+    print(locations)
 
     return locations
 
@@ -78,6 +82,9 @@ def test():
     locations = extract_locations(
         "Auckland commuters warned to take ‘extra care’ on Harbour Bridge due to strong wind gusts ",
         "https://www.nzherald.co.nz/nz/auckland-traffic-builds-as-school-holidays-begin-significant-road-works-in-waikato-continue/3TE4RVBKD5FXLILW6AHXDHXVFM/")
+
+    # TODO geocode using Nominatim HTTP API
+    # This is because geopy doesn't provide address rank
 
 
 if __name__ == "__main__":
