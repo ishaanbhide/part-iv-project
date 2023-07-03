7e67ee7c39c1232172c000bd58f4b57298890bd9
Matthew Ouyang <mouyang2001@gmail.com>
2023-07-03 15:29:55 +1200

    create post news functionality

--- pipeline/main.py
+++ pipeline/main.py
@@ -1,5 +1,6 @@
 import re
 import time
+from typing import List
 
 import feedparser
 import openai
@@ -8,13 +9,11 @@ import spacy
 from bs4 import BeautifulSoup
 from geopy.geocoders import Nominatim
 
+SERVER_NEWS_API = "http://localhost:3001/api/news"
 NZ_HERALD_RSS = "https://www.nzherald.co.nz/arc/outboundfeeds/rss/section/nz/?outputType=xml&_website=nzh"
 openai.api_key = "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"
 nlp = spacy.load("en_core_web_sm")
-geocoder = Nominatim(user_agent="wouy448@aucklanduni.ac.nz")
-
-ARTICLE_WORD_LIMIT = 100
-LOCATIONS_LIMIT = 5
+geocoder = Nominatim(user_agent="jameswood@gmail.com")
 
 
 def get_disasters():
@@ -25,7 +24,7 @@ def get_disasters():
     for entry in feed.entries:
         clean_title = re.sub(r"[^a-zA-Z0-9\s]", "", entry.title)
 
-        prompt = f'"""{clean_title}""" classify news headline into NATURAL DISASTER, ACCIDENTS, WEATHER or OTHER'
+        prompt = f'"""{clean_title}""" classify news headline into NATURAL DISASTER, WEATHER or OTHER'
         completion = openai.ChatCompletion.create(
             model="gpt-3.5-turbo",
             messages=[{"role": "user", "content": prompt}],
@@ -36,7 +35,7 @@ def get_disasters():
         response = completion.choices[0].message.content.upper()
         tokens += completion.usage.total_tokens
 
-        if "NATURAL DISASTER" in response or "ACCIDENTS" in response or "WEATHER" in response:
+        if "NATURAL DISASTER" in response or "WEATHER" in response:
             disasters.append((entry.title, entry.link))
 
         print(clean_title, ":", completion.choices[0].message.content)
@@ -47,20 +46,23 @@ def get_disasters():
     return disasters
 
 
-def scrape_article(link: str):
+def scrape_article(link: str, word_limit=100) -> str:
     r = requests.get(link)
     soup = BeautifulSoup(r.text, 'html.parser')
 
     text = soup.find_all('p')
     article_raw = " ".join([paragraph.text for paragraph in text])
-    article_truncated = " ".join(article_raw.split()[:ARTICLE_WORD_LIMIT])
+    article_truncated = " ".join(article_raw.split()[:word_limit])
     article = re.sub(r"[^a-zA-Z0-9\s]", "", article_truncated)
+    print(article)
+
+    # TODO: scrape images, from dynamic website using selenium
 
     return article
 
 
-def extract_potential_locations(article: str):
-    prompt = f'"""{article}""" extract location data from text as bullet points'
+def extract_locations(article: str, limit=1) -> List[str]:
+    prompt = f'"""{article}""" extract searchable locations from text as bullet points. Direct answer.'
     completion = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=[{"role": "user", "content": prompt}],
@@ -69,32 +71,58 @@ def extract_potential_locations(article: str):
     )
 
     locations = re.findall(r'- (.+)', completion.choices[0].message.content)
-
-    print(completion.choices[0].message.content)
-    print("Total tokens used:", completion.usage.total_tokens)
     print(locations)
+    print("Total tokens used:", completion.usage.total_tokens)
 
-    return locations[:LOCATIONS_LIMIT]
+    return locations[:limit]
 
 
-def geocode(location: str):
-    response = geocoder.geocode(f"{location} New Zealand")
-    time.sleep(1)
-    return response.raw if response else None
+def geocode_locations(locations: List[str]):
+    res = []
+    for location in locations:
+        response = geocoder.geocode(f"{location} New Zealand")
+        if response:
+            res.append(response)
+
+        time.sleep(1)
+
+    return res
+
+
+def post_news(title: str, article: str, longitude: float, latitude: float) -> None:
+    news = {
+        "title": title,
+        "body": article,
+        "source": "NZ Herald",
+        "image": "null",
+        "location": {
+            "type": "Point",
+            "coordinates": [longitude, latitude]
+        }
+    }
+
+    response = requests.post(SERVER_NEWS_API, json=news)
+    if response.status_code in [200, 201]:
+        print('Post successful:', response.json())
+    else:
+        print('Post unsuccessful:', response.text)
 
 
 def main():
     disasters = get_disasters()
     for title, link in disasters:
         article = scrape_article(link)
-        locations = extract_potential_locations(article)
-        geocoded_locations = list(filter(None, (geocode(location) for location in locations)))
-        if geocoded_locations:
-            best_location = min(geocoded_locations, key=lambda loc: loc['importance'])
-            print(best_location['display_name'])
-        else:
-            print("No locations found")
+        locations = extract_locations(article)
+        geocoded_locations = geocode_locations(locations)
+        for geocoded_location in geocoded_locations:
+            post_news(title, article, geocoded_location.longitude, geocoded_location.latitude)
+
+
+def test():
+    scrape_article(
+        "https://www.nzherald.co.nz/talanoa/warning-of-more-cyclones-for-parts-of-pacific-as-el-nino-threatens/QZU7IQLZ2ZF4DD3EGEBOOXAPDY/")
 
 
 if __name__ == "__main__":
-    main()
+    # main()
+    test()
