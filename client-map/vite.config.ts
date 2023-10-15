ef91a903821e91028a58ec920a768facd34d861f
Matthew Ouyang <mouyang2001@gmail.com>
2023-10-15 16:15:47 +1300

    prepare for pwa

--- client-map/vite.config.ts
+++ client-map/vite.config.ts
@@ -1,7 +1,37 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [
+        react(),
+        VitePWA({
+            manifest: {
+                // content of manifest file
+                name: "Geohub",
+                short_name: "Geobhub",
+                start_url: "/",
+                display: "standalone",
+                background_color: "#000000",
+                theme_color: "#000000",
+                description: "Part IV Project Disaster News App.",
+                icons: [
+                    {
+                        src: "logo-192x192.png",
+                        sizes: "192x192",
+                        type: "image/png",
+                    },
+                    {
+                        src: "logo-512x512.png",
+                        sizes: "512x512",
+                        type: "image/png",
+                    },
+                ],
+            },
+            workbox: {
+                // workbox options for service worker
+            },
+        }),
+    ],
 });
